a259ef5282a76cc0a81d6124ff119fe4c59e0f70
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-07-20 17:53:13 +0200

    Added Loader Component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Grid } from "react-loader-spinner";
 
 export default function Weather() {
-	return <p>Hello from Weather!</p>;
+	return (
+		<div className="Weather">
+			<p>Hello from Weather!</p>
+			<Grid height="100" width="100" color="grey" ariaLabel="loading" />
+		</div>
+	);
 }
