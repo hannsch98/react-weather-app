012e7b25930e3d81f3cd42507f929ceaf7554db9
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-07-20 18:07:26 +0200

    Added Bootstrap, CSS, Weather & Search Component

---
+++ src/Search.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+
+export default function Search() {
+	return (
+		<div className="Search">
+			<form id="search-form">
+				<label htmlFor="search-input" id="search-label">
+					Search for a city:
+				</label>
+				<input
+					type="text"
+					placeholder="Enter city here"
+					className="form-control"
+					id="search-input"
+					autoComplete="off"
+				/>
+				<input type="Submit" value="Search" className="btn btn-primary" />
+				<button id="current-location" className="btn btn-secondary">
+					My location
+				</button>
+			</form>
+		</div>
+	);
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,36 @@
 import React from "react";
-import { Grid } from "react-loader-spinner";
+import Search from "./Search";
+import "./styles.css";
 
 export default function Weather() {
 	return (
 		<div className="Weather">
-			<p>Hello from Weather!</p>
-			<Grid height="100" width="100" color="grey" ariaLabel="loading" />
+			<div className="row">
+				<div className="col-8">
+					<p>Current weather in</p>
+					<div className="row">
+						<div className="col-6">
+							<h2 id="current-city">Vienna</h2>
+							<p>
+								<span id="current-temp">25 °C</span>
+								<img
+									src="http://openweathermap.org/img/wn/01d@2x.png"
+									id="weather-icon"
+									alt="clear sky"
+								/>
+							</p>
+						</div>
+						<div className="col-6">
+							<p id="weather-description">Sunny</p>
+							<p id="weather-wind">Wind 5 km/h</p>
+							<p id="weather-humid">Humidity 35 %</p>
+						</div>
+					</div>
+				</div>
+				<div className="col-4" id="search-box">
+					<Search />
+				</div>
+			</div>
 		</div>
 	);
 }
