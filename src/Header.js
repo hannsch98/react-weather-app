1e48b0b174ebc91d5cafdc9769b6358fad5a047e
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-07-21 20:42:57 +0200

    Added current date function to Header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,51 @@
 import React from "react";
 
 export default function Header() {
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+
+	let months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+
+	//set date to current date
+	let currentTime = new Date();
+	let currentWeekDay = days[currentTime.getDay()];
+	let currentDate = currentTime.getDate();
+	let currentMonth = months[currentTime.getMonth()];
+	let currentYear = currentTime.getFullYear();
+	//set time to current time
+	let currentHour = currentTime.getHours();
+	let currentMinutes = currentTime.getMinutes();
+	if (currentMinutes < 10) {
+		currentMinutes = `0${currentMinutes}`;
+	}
+
 	return (
 		<div className="Header">
 			<h1>The Weather</h1>
-			<p id="current-date">Wednesday, 20. July 2022, 18:10</p>
+			<p id="current-date">
+				{currentWeekDay}, {currentDate}. {currentMonth} {currentYear},{" "}
+				{currentHour}:{currentMinutes}
+			</p>
 		</div>
 	);
 }
