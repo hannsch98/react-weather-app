28ab762d38d0694b86a9db310f07b7ebc6b91169
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-08-01 16:17:49 +0200

    Updated display of 0 for numbers below 10

--- src/Header.js
+++ src/Header.js
@@ -26,18 +26,25 @@ export default function Header() {
 		"December",
 	];
 
-	//set date to current date
+	//set date & time to current one
 	let currentTime = new Date();
 	let currentWeekDay = days[currentTime.getDay()];
+
 	let currentDate = currentTime.getDate();
+	//ternary if-statement:
+	currentDate < 10 ? (currentDate = `0${currentDate}`) : currentDate;
+
 	let currentMonth = months[currentTime.getMonth()];
 	let currentYear = currentTime.getFullYear();
-	//set time to current time
+
 	let currentHour = currentTime.getHours();
+	currentHour < 10 ? (currentHour = `0${currentHour}`) : currentHour;
+
 	let currentMinutes = currentTime.getMinutes();
 	if (currentMinutes < 10) {
 		currentMinutes = `0${currentMinutes}`;
 	}
+	//currentMinutes<10?currentMinutes=`0${currentMinutes}`:currentMinutes;
 
 	return (
 		<div className="Header">
