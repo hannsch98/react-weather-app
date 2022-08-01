1bf66a57ddd4e04d0074596a3fcfdaef74cd3555
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-08-01 15:28:27 +0200

    Added class of Weather, autofocus on Search and CSS file

--- src/Weather.js
+++ src/Weather.js
@@ -24,7 +24,7 @@ export default function Weather() {
 
 	function showWeather(response) {
 		setLoaded(true);
-		console.log(response.data);
+		//console.log(response.data);
 
 		// Set weather as an object
 		setWeather({
@@ -131,6 +131,7 @@ export default function Weather() {
 				className="form-control"
 				id="search-input"
 				autoComplete="off"
+				autoFocus="on"
 				onChange={updateInput}
 			/>
 			<button
@@ -177,52 +178,56 @@ export default function Weather() {
 
 	if (loaded) {
 		return (
-			<div className="container">
-				<div className="row">
-					{/* Current weather */}
-					<div className="col-8">
-						<p>Current weather in</p>
-						<div className="row">
-							<div className="col-6">
-								<h2 id="current-city">{weather.city}</h2>
-								<p>
-									<span id="current-temp">{weather.temp} °C</span>
-									<img
-										src={weather.icon}
-										id="weather-icon"
-										alt="Weather Icon"
-									/>
-								</p>
-							</div>
-							<div className="col-6">
-								<p id="weather-description">{weather.description}</p>
-								<p id="weather-wind">Wind {weather.wind} km/h</p>
-								<p id="weather-humid">Humidity {weather.humidity} %</p>
+			<div className="Weather">
+				<div className="container">
+					<div className="row">
+						{/* Current weather */}
+						<div className="col-8">
+							<p>Current weather in</p>
+							<div className="row">
+								<div className="col-6">
+									<h2 id="current-city">{weather.city}</h2>
+									<p>
+										<span id="current-temp">{weather.temp} °C</span>
+										<img
+											src={weather.icon}
+											id="weather-icon"
+											alt="Weather Icon"
+										/>
+									</p>
+								</div>
+								<div className="col-6">
+									<p id="weather-description">{weather.description}</p>
+									<p id="weather-wind">Wind {weather.wind} km/h</p>
+									<p id="weather-humid">Humidity {weather.humidity} %</p>
+								</div>
 							</div>
 						</div>
-					</div>
 
-					{/* Search Box */}
-					<div className="col-4" id="search-box">
-						{searchForm}
+						{/* Search Box */}
+						<div className="col-4" id="search-box">
+							{searchForm}
+						</div>
 					</div>
+					<Forecast />
 				</div>
-				<Forecast />
 			</div>
 		);
 	} else {
 		return (
-			<div className="container">
-				<div className="row">
-					{/* Current weather */}
-					{defaultWeather}
+			<div className="Weather">
+				<div className="container">
+					<div className="row">
+						{/* Current weather */}
+						{defaultWeather}
 
-					{/* Search Box */}
-					<div className="col-4" id="search-box">
-						{searchForm}
+						{/* Search Box */}
+						<div className="col-4" id="search-box">
+							{searchForm}
+						</div>
 					</div>
+					<Forecast />
 				</div>
-				<Forecast />
 			</div>
 		);
 	}
