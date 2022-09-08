6a984f1f00703657b44687f9661622675141d549
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-09-08 09:34:25 +0200

    Refactoring Header

--- src/Header.js
+++ src/Header.js
@@ -1,54 +1,71 @@
 import React from "react";
 import "./Header.css";
 
-export default function Header() {
-	let days = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	];
+const days = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
 
-	let months = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 
-	//set date & time to current one
-	let currentTime = new Date();
-
-	let currentWeekDay = days[currentTime.getDay()];
-
+function getCurrentTime() {
+	const currentTime = new Date();
+	const currentWeekDay = days[currentTime.getDay()];
+	const currentMonth = months[currentTime.getMonth()];
+	const currentYear = currentTime.getFullYear();
 	let currentDate = currentTime.getDate();
+	let currentHour = currentTime.getHours();
+	let currentMinutes = currentTime.getMinutes();
+
 	if (currentDate < 10) {
 		currentDate = `0${currentDate}`;
 	}
 
-	let currentMonth = months[currentTime.getMonth()];
-	let currentYear = currentTime.getFullYear();
-
-	let currentHour = currentTime.getHours();
 	if (currentHour < 10) {
 		currentHour = `0${currentHour}`;
 	}
 
-	let currentMinutes = currentTime.getMinutes();
 	if (currentMinutes < 10) {
 		currentMinutes = `0${currentMinutes}`;
 	}
+
+	return [
+		currentWeekDay,
+		currentDate,
+		currentMonth,
+		currentYear,
+		currentHour,
+		currentMinutes,
+	];
+}
+
+export default function Header() {
+	const [
+		currentWeekDay,
+		currentDate,
+		currentMonth,
+		currentYear,
+		currentHour,
+		currentMinutes,
+	] = getCurrentTime();
 
 	return (
 		<div className="Header">
